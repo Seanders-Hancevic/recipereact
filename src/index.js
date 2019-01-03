5c52faa31fb160aa89b11bcebe2b36fbaf6c4b3f
Seanders Hancevic <seanhance1@gmail.com>
2019-01-03 18:39:21 -0500

    stying

--- src/index.js
+++ src/index.js
@@ -1,37 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import logo from './assets/cutlery-circle.png'
 
+
+const Header = (props) => (
+    <div className='header'>
+        <img className ='logo' src={logo} alt='Cutlery'></img>
+        <h1 className ='headerText'>Recipe App</h1>
+    </div>
+ )
 
 const DirectoryView = (props) => (
     <div>
-        <SearchForm value={props.value} handleChange={props.changeHandler} recipeSelect={props.recipeSelect} />
-        {props.recipes.map((recipe, i) => <RecipeCard name={recipe.name} key={i} />)}
+        <SearchForm value={props.value} handleChange={props.changeHandler} clickHandler={props.clickHandler} recipeSelect={props.recipeSelect} />
+        {props.recipes.map((recipe, i) => <RecipeCard name={recipe.name} id={recipe.id} clickHandler={props.recipeHandler} key={i} />)}
 
     </div>
 )
 
 
 const SearchForm = (props) => (
-    <form>
-        <input value={props.value} onChange={props.handleChange} />
-        <button onClick={props.recipeSelect}>Search</button>
+    <form className = 'search'>
+        <input className ='inputField' value={props.value} onChange={props.handleChange} />
+        <button className = 'searchButton' onClick={props.recipeSelect}>Search</button>
     </form>
 );
 
 const RecipeCard = (props) => (
     <div>
-        <h1>Recipe Name</h1>
-        <p>{props.name}</p>
+ <div className = 'recipeCard'>
+        <h1 onClick={() => props.clickHandler(props.id)}>{props.name}</h1>
     </div>
+    </div>
+   
 )
 
 const DetailView = (props) => (
 
-    <div>
- {props.recipes.filter((recipe) => <h2 key ={[0]}>{recipe.name}</h2>)}
-        <h2>{props.name}</h2>
-        {/* <p>{props.recipes.instructions}</p> */}
+    <div className ='container'>
+        <h2>{props.recipes.name}</h2>
+        <div className="">
+            <ul>
+                {props.recipes.ingredients.map((ingredient, i) => <li key={i}>{ingredient}</li>)}
+            </ul>
+        </div>
+        <div className="">
+            <ol>
+                {props.recipes.instructions.map((instruction, i) => <li key={i}>{instruction}</li>)}
+            </ol>
+        </div>
     </div>
 )
 
@@ -79,11 +97,64 @@ class App extends React.Component {
                 name: 'Whole Turkey',
                 ingredients: ['1 (12 pound) whole turkey', '6 tablespoons butter, divided', '4 cups warm water', '3 tablespoons chicken bouillon', '2 tablespoons dried parsley', '2 tablespoons dried minced onion', '2 tablespoons seasoning salt'],
                 instructions: ["Preheat oven to 350 degrees F (175 degrees C). Rinse and wash turkey. Discard the giblets, or add to pan if they are anyone's favorites.", 'Place turkey in a Dutch oven or roasting pan. Separate the skin over the breast to make little pockets. Put 3 tablespoons of the butter on both sides between the skin and breast meat. This makes for very juicy breast meat.', 'In a medium bowl, combine the water with the bouillon. Sprinkle in the parsley and minced onion. Pour over the top of the turkey. Sprinkle seasoning salt over the turkey.', 'Cover with foil, and bake in the preheated oven 3 1/2 to 4 hours, until the internal temperature of the turkey reaches 180 degrees F (80 degrees C). For the last 45 minutes or so, remove the foil so the turkey will brown nicely.']
-            }
+            },
+            {
+                id: 7,
+                name: 'Turkey Mayhem',
+                ingredients: ['Turkey', 'Mustard', 'Greens', 'Celery', 'Potato'],
+                instructions: ['Pre-heat over to 350 degrees', 'Tickle the turkey', 'Cut some celery']
+            },
+            {
+                id: 8,
+                name: 'Mac and Cheese',
+                ingredients: ['Mac', 'Cheese'],
+                instructions: ['Put the mac in the cheese', 'Swish it around', 'Bake for 15 minutes at 350 degrees']
+            },
+            {
+                id: 9,
+                name: 'Spanakopita',
+                ingredients: ['Olive oil', 'Onion', 'Garlic', 'Spinach'],
+                instructions: ['Put the stuff in a bowl', 'Cook the stuff']
+            },
+            {
+                id: 10,
+                name: 'Vasilopita',
+                ingredients: ['Flower', 'Baking Soda', 'Butter', 'Sugar', 'Eggs'],
+                instructions: ['Throw everything in the air and hope it comes together', 'Knead the bread', 'Show the bread some love', 'Bake it at 400 degrees'],
+            },
+            {
+                id: 11,
+                name: 'Ceviche',
+                ingredients: ['tilapia', 'cilantro', 'tomatoes', 'yellow onion', 'limes', 'jalapeno', 'salt'],
+                instructions: ['Dice all tilapia and place in a bowl.','Squeeze lime juice over all the tilapia (enough to cover it) and add salt.', 'Let the tilapia sit in lime juice for about 2 hrs.', 'Dice all other ingredients and set aside.',  'After 2 hrs, mix tilapia and diced ingredients.', 'Enjoy with tortilla chips or tostadas :)']
+               },
+               {
+                id: 12,
+                name: 'Tofu Lasagna',
+                ingredients: ['tofu', 'cheese', 'noodles', 'tomato sauce',],
+                instructions: ['pre-heat oven to 400', 'marinade tofu to taste', 'cook noodles', 'mix tomato sauce and tofu', 'put a layer of tofu/sauce in the pan', 'put a layer of noodles', 'repeat until all ingredients are used', 'We dont waste in this house', 'cook until bubbling'],
+            },
+            {
+                id:13,
+                name: 'Tofu Potpie',
+                ingredients: ['tofu', 'pie crust', 'assorted veggies', 'gravy sauce'],
+                instructions: ['pre-heat oven to 400', 'line pan with pie crust', 'mix tofu, veggies and gravy sauce', 'put tofu mix in pie crust', 'cook until golden brown'],
+            },
+            { id: 14, 
+                name: 'Turkey Tacos', 
+                ingredients: ['1 lb ground turkey', '1 head romaine lettuce', '1 avacado', '1 red bell pepper chopped', '1 bunch green onions', '1 tbsp chili powder', '1/4 tsp garlic powder', '1/8 tsp onion powder', '1/4 tsp crushed red pepper flakes', '1/4 tsp dried oregano', '1/2 tsp paprika', '1/2 tsp ground cumin', '1/2 tsp sea salt', '1/2 tsp black pepper', 'cilantro for garnish'], 
+                instructions: ['1. Brown turkey in skillet and add spices.', '2. Separate Romaine lettuce leaves and layer with taco meat, green onions, red pepper, sliced avacado and cilantro.', '3. Serve with hot sauce or salsa if desired.'] 
+              },
         ],
 
         searchVal: '',
-        selectedRecipe: []
+        selectedRecipe: [],
+        chosenRecipe: {
+            id: '',
+            name: '',
+            ingredients: [],
+            instructions: []
+        }
 
     }
 
@@ -93,27 +164,36 @@ class App extends React.Component {
 
     selectedRecipe = (event) => {
         event.preventDefault()
-        const filteredRecipies = this.state.recipes.filter(recipe => recipe.name.includes(this.state.searchVal))
+        const filteredRecipies = this.state.recipes.filter(recipe => recipe.name.toLowerCase().includes(this.state.searchVal.toLowerCase()))
         this.setState({ selectedRecipe: filteredRecipies });
     }
 
+    chooseRecipe = (id) => {
+        this.setState({ chosenRecipe: this.state.recipes.find(recipe => recipe.id === id)})
+    }
 
     render() {
         const recipeRef = this.state.selectedRecipe
         return (
             <div>
-                <DirectoryView recipes={recipeRef}
-                    value={this.state.searchVal}
-                    changeHandler={this.handleChange}
-                    recipeSelect={this.selectedRecipe}
-                />
-                <DetailView recipes={recipeRef[0]} 
-          
-                />
+                <Header />
+                <div className ='cardContainer'>
+                    <DirectoryView
+                        recipes={recipeRef}
+                        value={this.state.searchVal}
+                        changeHandler={this.handleChange}
+                        recipeSelect={this.selectedRecipe} 
+                        clickHandler={this.selectRecipes} 
+                        recipeHandler={this.chooseRecipe}
+                    />
+                </div>
+                <div className ='detailContainer'>
+                    <DetailView recipes={this.state.chosenRecipe}
+                    />
+                </div>
             </div>
         )
-    }
-
+ }
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));

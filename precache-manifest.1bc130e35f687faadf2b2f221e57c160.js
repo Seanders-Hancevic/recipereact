4b3a252a441d5c1210a25b78e913d5606d13c566
Seanders Hancevic <seanhance1@gmail.com>
2019-01-03 19:22:50 -0500

    Updates

---
+++ precache-manifest.1bc130e35f687faadf2b2f221e57c160.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aa3e92d069fc5d535b53",
+    "url": "/recipereact/static/js/runtime~main.aa3e92d0.js"
+  },
+  {
+    "revision": "835e8ca82fe22a823b4e",
+    "url": "/recipereact/static/js/main.835e8ca8.chunk.js"
+  },
+  {
+    "revision": "af2c9dd9ba61e82900a1",
+    "url": "/recipereact/static/js/1.af2c9dd9.chunk.js"
+  },
+  {
+    "revision": "835e8ca82fe22a823b4e",
+    "url": "/recipereact/static/css/main.1e6d23f2.chunk.css"
+  },
+  {
+    "revision": "6c3f778f364d5a4c975630ac79ec1c25",
+    "url": "/recipereact/index.html"
+  }
+];
